f45708d6ef8783cd009a0dc213098485adefcec0
Teslenko A.A.(work) <alteslo31@gmail.com>
2021-12-03 17:54:33 +0300

    some changes

---
+++ web/hello.py
@@ -0,0 +1,10 @@
+
+
+def app(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+
+    body = '\r\n'.join(environ['QUERY_STRING'].split('&'))
+    start_response(status, headers)
+
+    return [bytes(body, encoding='utf-8')]
